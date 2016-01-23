2dd3d767b0725d78ae41fb20c41a482d2d977afa
hellothereamy <trannguyen.amy@gmail.com>
2016-01-22 21:35:15 -0800

    added fresh planly app

---
+++ planly/dist/assets/planly.js
@@ -0,0 +1,449 @@
+"use strict";
+/* jshint ignore:start */
+
+/* jshint ignore:end */
+
+define('planly/adapters/application', ['exports', 'ember', 'emberfire/adapters/firebase'], function (exports, _ember, _emberfireAdaptersFirebase) {
+  var inject = _ember['default'].inject;
+  exports['default'] = _emberfireAdaptersFirebase['default'].extend({
+    firebase: inject.service()
+  });
+});
+define('planly/app', ['exports', 'ember', 'ember/resolver', 'ember/load-initializers', 'planly/config/environment'], function (exports, _ember, _emberResolver, _emberLoadInitializers, _planlyConfigEnvironment) {
+
+  var App = undefined;
+
+  _ember['default'].MODEL_FACTORY_INJECTIONS = true;
+
+  App = _ember['default'].Application.extend({
+    modulePrefix: _planlyConfigEnvironment['default'].modulePrefix,
+    podModulePrefix: _planlyConfigEnvironment['default'].podModulePrefix,
+    Resolver: _emberResolver['default']
+  });
+
+  (0, _emberLoadInitializers['default'])(App, _planlyConfigEnvironment['default'].modulePrefix);
+
+  exports['default'] = App;
+});
+define('planly/components/app-version', ['exports', 'ember-cli-app-version/components/app-version', 'planly/config/environment'], function (exports, _emberCliAppVersionComponentsAppVersion, _planlyConfigEnvironment) {
+
+  var name = _planlyConfigEnvironment['default'].APP.name;
+  var version = _planlyConfigEnvironment['default'].APP.version;
+
+  exports['default'] = _emberCliAppVersionComponentsAppVersion['default'].extend({
+    version: version,
+    name: name
+  });
+});
+define('planly/controllers/array', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = _ember['default'].Controller;
+});
+define('planly/controllers/object', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = _ember['default'].Controller;
+});
+define('planly/initializers/app-version', ['exports', 'ember-cli-app-version/initializer-factory', 'planly/config/environment'], function (exports, _emberCliAppVersionInitializerFactory, _planlyConfigEnvironment) {
+  exports['default'] = {
+    name: 'App Version',
+    initialize: (0, _emberCliAppVersionInitializerFactory['default'])(_planlyConfigEnvironment['default'].APP.name, _planlyConfigEnvironment['default'].APP.version)
+  };
+});
+define('planly/initializers/emberfire', ['exports', 'emberfire/initializers/emberfire'], function (exports, _emberfireInitializersEmberfire) {
+  exports['default'] = _emberfireInitializersEmberfire['default'];
+});
+define('planly/initializers/export-application-global', ['exports', 'ember', 'planly/config/environment'], function (exports, _ember, _planlyConfigEnvironment) {
+  exports.initialize = initialize;
+
+  function initialize() {
+    var application = arguments[1] || arguments[0];
+    if (_planlyConfigEnvironment['default'].exportApplicationGlobal !== false) {
+      var value = _planlyConfigEnvironment['default'].exportApplicationGlobal;
+      var globalName;
+
+      if (typeof value === 'string') {
+        globalName = value;
+      } else {
+        globalName = _ember['default'].String.classify(_planlyConfigEnvironment['default'].modulePrefix);
+      }
+
+      if (!window[globalName]) {
+        window[globalName] = application;
+
+        application.reopen({
+          willDestroy: function willDestroy() {
+            this._super.apply(this, arguments);
+            delete window[globalName];
+          }
+        });
+      }
+    }
+  }
+
+  exports['default'] = {
+    name: 'export-application-global',
+
+    initialize: initialize
+  };
+});
+define('planly/initializers/initialize-torii-callback', ['exports', 'torii/redirect-handler'], function (exports, _toriiRedirectHandler) {
+  exports['default'] = {
+    name: 'torii-callback',
+    before: 'torii',
+    initialize: function initialize(application) {
+      if (arguments[1]) {
+        // Ember < 2.1
+        application = arguments[1];
+      }
+      application.deferReadiness();
+      _toriiRedirectHandler['default'].handle(window)['catch'](function () {
+        application.advanceReadiness();
+      });
+    }
+  };
+});
+define('planly/initializers/initialize-torii-session', ['exports', 'torii/configuration', 'torii/bootstrap/session'], function (exports, _toriiConfiguration, _toriiBootstrapSession) {
+  exports['default'] = {
+    name: 'torii-session',
+    after: 'torii',
+
+    initialize: function initialize(application) {
+      if (arguments[1]) {
+        // Ember < 2.1
+        application = arguments[1];
+      }
+      if (_toriiConfiguration['default'].sessionServiceName) {
+        (0, _toriiBootstrapSession['default'])(application, _toriiConfiguration['default'].sessionServiceName);
+
+        var sessionFactoryName = 'service:' + _toriiConfiguration['default'].sessionServiceName;
+        application.inject('adapter', _toriiConfiguration['default'].sessionServiceName, sessionFactoryName);
+      }
+    }
+  };
+});
+define('planly/initializers/initialize-torii', ['exports', 'torii/bootstrap/torii', 'torii/configuration'], function (exports, _toriiBootstrapTorii, _toriiConfiguration) {
+
+  var initializer = {
+    name: 'torii',
+    initialize: function initialize(application) {
+      if (arguments[1]) {
+        // Ember < 2.1
+        application = arguments[1];
+      }
+      (0, _toriiBootstrapTorii['default'])(application);
+      application.inject('route', 'torii', 'service:torii');
+    }
+  };
+
+  if (window.DS) {
+    initializer.after = 'store';
+  }
+
+  exports['default'] = initializer;
+});
+define('planly/instance-initializers/setup-routes', ['exports', 'torii/configuration', 'torii/bootstrap/routing', 'torii/router-dsl-ext'], function (exports, _toriiConfiguration, _toriiBootstrapRouting, _toriiRouterDslExt) {
+  exports['default'] = {
+    name: 'torii-setup-routes',
+    initialize: function initialize(applicationInstance, registry) {
+      if (_toriiConfiguration['default'].sessionServiceName) {
+        var router = applicationInstance.get('router');
+        var setupRoutes = function setupRoutes() {
+          var authenticatedRoutes = router.router.authenticatedRoutes;
+          var hasAuthenticatedRoutes = !Ember.isEmpty(authenticatedRoutes);
+          if (hasAuthenticatedRoutes) {
+            (0, _toriiBootstrapRouting['default'])(applicationInstance, authenticatedRoutes);
+          }
+          router.off('willTransition', setupRoutes);
+        };
+        router.on('willTransition', setupRoutes);
+      }
+    }
+  };
+});
+define('planly/instance-initializers/walk-providers', ['exports', 'torii/configuration', 'torii/lib/container-utils'], function (exports, _toriiConfiguration, _toriiLibContainerUtils) {
+  exports['default'] = {
+    name: 'torii-walk-providers',
+    initialize: function initialize(applicationInstance) {
+      // Walk all configured providers and eagerly instantiate
+      // them. This gives providers with initialization side effects
+      // like facebook-connect a chance to load up assets.
+      for (var key in _toriiConfiguration['default'].providers) {
+        if (_toriiConfiguration['default'].providers.hasOwnProperty(key)) {
+          (0, _toriiLibContainerUtils.lookup)(applicationInstance, 'torii-provider:' + key);
+        }
+      }
+    }
+  };
+});
+define('planly/router', ['exports', 'ember', 'planly/config/environment'], function (exports, _ember, _planlyConfigEnvironment) {
+
+  var Router = _ember['default'].Router.extend({
+    location: _planlyConfigEnvironment['default'].locationType
+  });
+
+  Router.map(function () {});
+
+  exports['default'] = Router;
+});
+define('planly/services/firebase', ['exports', 'emberfire/services/firebase', 'planly/config/environment'], function (exports, _emberfireServicesFirebase, _planlyConfigEnvironment) {
+
+  _emberfireServicesFirebase['default'].config = _planlyConfigEnvironment['default'];
+
+  exports['default'] = _emberfireServicesFirebase['default'];
+});
+define("planly/templates/application", ["exports"], function (exports) {
+  exports["default"] = Ember.HTMLBars.template((function () {
+    return {
+      meta: {
+        "revision": "Ember@1.13.12",
+        "loc": {
+          "source": null,
+          "start": {
+            "line": 1,
+            "column": 0
+          },
+          "end": {
+            "line": 7,
+            "column": 29
+          }
+        },
+        "moduleName": "planly/templates/application.hbs"
+      },
+      arity: 0,
+      cachedFragment: null,
+      hasRendered: false,
+      buildFragment: function buildFragment(dom) {
+        var el0 = dom.createDocumentFragment();
+        var el1 = dom.createComment("");
+        dom.appendChild(el0, el1);
+        var el1 = dom.createTextNode("\n\n");
+        dom.appendChild(el0, el1);
+        var el1 = dom.createElement("main");
+        var el2 = dom.createTextNode("\n	");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createComment("");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createTextNode("\n");
+        dom.appendChild(el1, el2);
+        dom.appendChild(el0, el1);
+        var el1 = dom.createTextNode("\n\n");
+        dom.appendChild(el0, el1);
+        var el1 = dom.createComment("");
+        dom.appendChild(el0, el1);
+        return el0;
+      },
+      buildRenderNodes: function buildRenderNodes(dom, fragment, contextualElement) {
+        var morphs = new Array(3);
+        morphs[0] = dom.createMorphAt(fragment, 0, 0, contextualElement);
+        morphs[1] = dom.createMorphAt(dom.childAt(fragment, [2]), 1, 1);
+        morphs[2] = dom.createMorphAt(fragment, 4, 4, contextualElement);
+        dom.insertBoundary(fragment, 0);
+        dom.insertBoundary(fragment, null);
+        return morphs;
+      },
+      statements: [["inline", "partial", ["partials/nav"], [], ["loc", [null, [1, 0], [1, 26]]]], ["content", "outlet", ["loc", [null, [4, 1], [4, 11]]]], ["inline", "partial", ["partials/footer"], [], ["loc", [null, [7, 0], [7, 29]]]]],
+      locals: [],
+      templates: []
+    };
+  })());
+});
+define("planly/templates/partials/-footer", ["exports"], function (exports) {
+  exports["default"] = Ember.HTMLBars.template((function () {
+    return {
+      meta: {
+        "revision": "Ember@1.13.12",
+        "loc": {
+          "source": null,
+          "start": {
+            "line": 1,
+            "column": 0
+          },
+          "end": {
+            "line": 16,
+            "column": 9
+          }
+        },
+        "moduleName": "planly/templates/partials/-footer.hbs"
+      },
+      arity: 0,
+      cachedFragment: null,
+      hasRendered: false,
+      buildFragment: function buildFragment(dom) {
+        var el0 = dom.createDocumentFragment();
+        var el1 = dom.createElement("footer");
+        dom.setAttribute(el1, "id", "foot");
+        dom.setAttribute(el1, "class", "page-footer  indigo");
+        var el2 = dom.createTextNode("\n  ");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createElement("div");
+        dom.setAttribute(el2, "class", "container");
+        var el3 = dom.createTextNode("\n    ");
+        dom.appendChild(el2, el3);
+        var el3 = dom.createElement("div");
+        dom.setAttribute(el3, "class", "row");
+        var el4 = dom.createTextNode("\n      ");
+        dom.appendChild(el3, el4);
+        var el4 = dom.createElement("div");
+        dom.setAttribute(el4, "class", "col l6 s12");
+        var el5 = dom.createTextNode("\n        ");
+        dom.appendChild(el4, el5);
+        var el5 = dom.createElement("h5");
+        dom.setAttribute(el5, "class", "white-text");
+        var el6 = dom.createTextNode("Footer Content");
+        dom.appendChild(el5, el6);
+        dom.appendChild(el4, el5);
+        var el5 = dom.createTextNode("\n        ");
+        dom.appendChild(el4, el5);
+        var el5 = dom.createElement("p");
+        dom.setAttribute(el5, "class", "grey-text text-lighten-4");
+        var el6 = dom.createTextNode("You can use rows and columns here to organize your footer content.");
+        dom.appendChild(el5, el6);
+        dom.appendChild(el4, el5);
+        var el5 = dom.createTextNode("\n      ");
+        dom.appendChild(el4, el5);
+        dom.appendChild(el3, el4);
+        var el4 = dom.createTextNode("\n    ");
+        dom.appendChild(el3, el4);
+        dom.appendChild(el2, el3);
+        var el3 = dom.createTextNode("\n  ");
+        dom.appendChild(el2, el3);
+        dom.appendChild(el1, el2);
+        var el2 = dom.createTextNode("\n  ");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createElement("div");
+        dom.setAttribute(el2, "class", "footer-copyright");
+        var el3 = dom.createTextNode("\n    ");
+        dom.appendChild(el2, el3);
+        var el3 = dom.createElement("div");
+        dom.setAttribute(el3, "class", "container");
+        var el4 = dom.createTextNode("\n      © 2014 Copyright Text\n      ");
+        dom.appendChild(el3, el4);
+        var el4 = dom.createElement("a");
+        dom.setAttribute(el4, "class", "grey-text text-lighten-4 right");
+        dom.setAttribute(el4, "href", "#!");
+        var el5 = dom.createTextNode("More Links");
+        dom.appendChild(el4, el5);
+        dom.appendChild(el3, el4);
+        var el4 = dom.createTextNode("\n    ");
+        dom.appendChild(el3, el4);
+        dom.appendChild(el2, el3);
+        var el3 = dom.createTextNode("\n  ");
+        dom.appendChild(el2, el3);
+        dom.appendChild(el1, el2);
+        var el2 = dom.createTextNode("\n");
+        dom.appendChild(el1, el2);
+        dom.appendChild(el0, el1);
+        return el0;
+      },
+      buildRenderNodes: function buildRenderNodes() {
+        return [];
+      },
+      statements: [],
+      locals: [],
+      templates: []
+    };
+  })());
+});
+define("planly/templates/partials/-nav", ["exports"], function (exports) {
+  exports["default"] = Ember.HTMLBars.template((function () {
+    return {
+      meta: {
+        "revision": "Ember@1.13.12",
+        "loc": {
+          "source": null,
+          "start": {
+            "line": 1,
+            "column": 0
+          },
+          "end": {
+            "line": 8,
+            "column": 7
+          }
+        },
+        "moduleName": "planly/templates/partials/-nav.hbs"
+      },
+      arity: 0,
+      cachedFragment: null,
+      hasRendered: false,
+      buildFragment: function buildFragment(dom) {
+        var el0 = dom.createDocumentFragment();
+        var el1 = dom.createElement("nav");
+        var el2 = dom.createTextNode("\n   ");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createElement("div");
+        dom.setAttribute(el2, "class", "nav-wrapper indigo");
+        var el3 = dom.createTextNode("\n     ");
+        dom.appendChild(el2, el3);
+        var el3 = dom.createElement("a");
+        dom.setAttribute(el3, "href", "#");
+        dom.setAttribute(el3, "class", "brand-logo");
+        var el4 = dom.createTextNode("Planly");
+        dom.appendChild(el3, el4);
+        dom.appendChild(el2, el3);
+        var el3 = dom.createTextNode("\n     ");
+        dom.appendChild(el2, el3);
+        var el3 = dom.createElement("ul");
+        dom.setAttribute(el3, "id", "nav-mobile");
+        dom.setAttribute(el3, "class", "right hide-on-med-and-down");
+        var el4 = dom.createTextNode("\n       ");
+        dom.appendChild(el3, el4);
+        var el4 = dom.createElement("li");
+        var el5 = dom.createTextNode(" ");
+        dom.appendChild(el4, el5);
+        var el5 = dom.createComment("");
+        dom.appendChild(el4, el5);
+        dom.appendChild(el3, el4);
+        var el4 = dom.createTextNode("\n     ");
+        dom.appendChild(el3, el4);
+        dom.appendChild(el2, el3);
+        var el3 = dom.createTextNode("\n   ");
+        dom.appendChild(el2, el3);
+        dom.appendChild(el1, el2);
+        var el2 = dom.createTextNode("\n ");
+        dom.appendChild(el1, el2);
+        dom.appendChild(el0, el1);
+        return el0;
+      },
+      buildRenderNodes: function buildRenderNodes(dom, fragment, contextualElement) {
+        var morphs = new Array(1);
+        morphs[0] = dom.createMorphAt(dom.childAt(fragment, [0, 1, 3, 1]), 1, 1);
+        return morphs;
+      },
+      statements: [["inline", "link-to", ["Sign In", "index"], [], ["loc", [null, [5, 12], [5, 41]]]]],
+      locals: [],
+      templates: []
+    };
+  })());
+});
+define('planly/torii-providers/firebase', ['exports', 'emberfire/torii-providers/firebase'], function (exports, _emberfireToriiProvidersFirebase) {
+  exports['default'] = _emberfireToriiProvidersFirebase['default'];
+});
+/* jshint ignore:start */
+
+/* jshint ignore:end */
+
+/* jshint ignore:start */
+
+define('planly/config/environment', ['ember'], function(Ember) {
+  var prefix = 'planly';
+/* jshint ignore:start */
+
+try {
+  var metaName = prefix + '/config/environment';
+  var rawConfig = Ember['default'].$('meta[name="' + metaName + '"]').attr('content');
+  var config = JSON.parse(unescape(rawConfig));
+
+  return { 'default': config };
+}
+catch(err) {
+  throw new Error('Could not read config from meta tag with name "' + metaName + '".');
+}
+
+/* jshint ignore:end */
+
+});
+
+if (!runningTests) {
+  require("planly/app")["default"].create({"name":"planly","version":"0.0.0+341e129c"});
+}
+
+/* jshint ignore:end */
+//# sourceMappingURL=planly.map
---
+++ planly/dist/assets/tests.js
@@ -0,0 +1,154 @@
+define('planly/tests/adapters/application.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - adapters');
+  QUnit.test('adapters/application.js should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'adapters/application.js should pass jshint.');
+  });
+});
+define('planly/tests/app.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - .');
+  QUnit.test('app.js should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'app.js should pass jshint.');
+  });
+});
+define('planly/tests/helpers/destroy-app', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = destroyApp;
+
+  function destroyApp(application) {
+    _ember['default'].run(application, 'destroy');
+  }
+});
+define('planly/tests/helpers/destroy-app.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - helpers');
+  QUnit.test('helpers/destroy-app.js should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/destroy-app.js should pass jshint.');
+  });
+});
+define('planly/tests/helpers/module-for-acceptance', ['exports', 'qunit', 'planly/tests/helpers/start-app', 'planly/tests/helpers/destroy-app'], function (exports, _qunit, _planlyTestsHelpersStartApp, _planlyTestsHelpersDestroyApp) {
+  exports['default'] = function (name) {
+    var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
+    (0, _qunit.module)(name, {
+      beforeEach: function beforeEach() {
+        this.application = (0, _planlyTestsHelpersStartApp['default'])();
+
+        if (options.beforeEach) {
+          options.beforeEach.apply(this, arguments);
+        }
+      },
+
+      afterEach: function afterEach() {
+        (0, _planlyTestsHelpersDestroyApp['default'])(this.application);
+
+        if (options.afterEach) {
+          options.afterEach.apply(this, arguments);
+        }
+      }
+    });
+  };
+});
+define('planly/tests/helpers/module-for-acceptance.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - helpers');
+  QUnit.test('helpers/module-for-acceptance.js should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/module-for-acceptance.js should pass jshint.');
+  });
+});
+define('planly/tests/helpers/resolver', ['exports', 'ember/resolver', 'planly/config/environment'], function (exports, _emberResolver, _planlyConfigEnvironment) {
+
+  var resolver = _emberResolver['default'].create();
+
+  resolver.namespace = {
+    modulePrefix: _planlyConfigEnvironment['default'].modulePrefix,
+    podModulePrefix: _planlyConfigEnvironment['default'].podModulePrefix
+  };
+
+  exports['default'] = resolver;
+});
+define('planly/tests/helpers/resolver.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - helpers');
+  QUnit.test('helpers/resolver.js should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/resolver.js should pass jshint.');
+  });
+});
+define('planly/tests/helpers/start-app', ['exports', 'ember', 'planly/app', 'planly/config/environment'], function (exports, _ember, _planlyApp, _planlyConfigEnvironment) {
+  exports['default'] = startApp;
+
+  function startApp(attrs) {
+    var application = undefined;
+
+    var attributes = _ember['default'].merge({}, _planlyConfigEnvironment['default'].APP);
+    attributes = _ember['default'].merge(attributes, attrs); // use defaults, but you can override;
+
+    _ember['default'].run(function () {
+      application = _planlyApp['default'].create(attributes);
+      application.setupForTesting();
+      application.injectTestHelpers();
+    });
+
+    return application;
+  }
+});
+define('planly/tests/helpers/start-app.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - helpers');
+  QUnit.test('helpers/start-app.js should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/start-app.js should pass jshint.');
+  });
+});
+define('planly/tests/helpers/torii', ['exports'], function (exports) {
+  exports.stubValidSession = stubValidSession;
+
+  function stubValidSession(application, sessionData) {
+    var session = application.__container__.lookup('service:session');
+    var sm = session.get('stateMachine');
+    Ember.run(function () {
+      sm.send('startOpen');
+      sm.send('finishOpen', sessionData);
+    });
+  }
+});
+define('planly/tests/router.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - .');
+  QUnit.test('router.js should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'router.js should pass jshint.');
+  });
+});
+define('planly/tests/test-helper', ['exports', 'planly/tests/helpers/resolver', 'ember-qunit'], function (exports, _planlyTestsHelpersResolver, _emberQunit) {
+
+  (0, _emberQunit.setResolver)(_planlyTestsHelpersResolver['default']);
+});
+define('planly/tests/test-helper.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - .');
+  QUnit.test('test-helper.js should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'test-helper.js should pass jshint.');
+  });
+});
+/* jshint ignore:start */
+
+require('planly/tests/test-helper');
+EmberENV.TESTS_FILE_LOADED = true;
+
+/* jshint ignore:end */
+//# sourceMappingURL=tests.map
