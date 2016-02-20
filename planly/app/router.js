f577a2a5bca6019af4e6ccac0174c28d88232f6f
betodt <alberto.diaz.tostado@gmail.com>
2016-02-20 15:48:24 -0800

    added route for each view

--- planly/app/router.js
+++ planly/app/router.js
@@ -6,6 +6,11 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('project', function() {
+    this.route('tasks');
+    this.route('calendar');
+    this.route('team');
+  });
 });
 
 export default Router;
---
+++ planly/tests/unit/routes/project/team-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:project/team', 'Unit | Route | project/team', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
